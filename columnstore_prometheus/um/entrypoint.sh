40e292e210644632a83f4868ce703fc517214012
Petko Vasilev <petko.vasilev@mariadb.com>
2018-07-11 12:08:23 +0300

    switched to k8s

--- columnstore_prometheus/um/entrypoint.sh
+++ columnstore_prometheus/um/entrypoint.sh
@@ -4,9 +4,9 @@
 /node_exporter/node_exporter > /dev/null 2>&1 &
 
 # start the columnstore install
-# /install/postCfg2.sh "2\n1\n\n\ncolumnstore-1\n\n1\nmariadb-cs-um\n\n\n2\nmariadb-cs-pm1\n\n\n\nmariadb-cs-pm2\n\n\n2\n\n\n"
+/install/postCfg2.sh "2\n1\n\n\ncolumnstore-1\n\n1\nmariadb-cs-um\n\n\n2\nmariadb-cs-pm1\n\n\n\nmariadb-cs-pm2\n\n\n2\n\n\n"
 
 # start the columnstore install
 # for docker-compose
-/install/postCfg2.sh "2\n1\n\n\ncolumnstore-1\n\n1\nmariadb-cs-um\n${UM_NODE_IP}\n\n2\nmariadb-cs-pm1\n${PM1_NODE_IP}\n\n\nmariadb-cs-pm2\n${PM2_NODE_IP}\n\n2\n\n\n"
+# /install/postCfg2.sh "2\n1\n\n\ncolumnstore-1\n\n1\nmariadb-cs-um\n${UM_NODE_IP}\n\n2\nmariadb-cs-pm1\n${PM1_NODE_IP}\n\n\nmariadb-cs-pm2\n${PM2_NODE_IP}\n\n2\n\n\n"
 
--- columnstore_prometheus/um/postCfg2.sh
+++ columnstore_prometheus/um/postCfg2.sh
@@ -5,10 +5,10 @@ export USER=root
 /bin/echo -e "$1" | /usr/local/mariadb/columnstore/bin/postConfigure -n
 
 # update root user to allow external connection, need to turn off NO_AUTO_CREATE_USER. 
-# ssh -vvv -o "StrictHostKeyChecking=no" -l root mariadb-cs-um '/usr/local/mariadb/columnstore/mysql/bin/mysql --defaults-file=/usr/local/mariadb/columnstore/mysql/my.cnf -uroot -vvv -Bse "set sql_mode=NO_ENGINE_SUBSTITUTION;GRANT ALL ON *.* to root@'"'"'%'"'"';FLUSH PRIVILEGES;"'
+ssh -vvv -o "StrictHostKeyChecking=no" -l root mariadb-cs-um '/usr/local/mariadb/columnstore/mysql/bin/mysql --defaults-file=/usr/local/mariadb/columnstore/mysql/my.cnf -uroot -vvv -Bse "set sql_mode=NO_ENGINE_SUBSTITUTION;GRANT ALL ON *.* to root@'"'"'%'"'"';FLUSH PRIVILEGES;"'
 
 # for docker-compose
-ssh -vvv -o "StrictHostKeyChecking=no" -l root ${UM_NODE_IP} '/usr/local/mariadb/columnstore/mysql/bin/mysql --defaults-file=/usr/local/mariadb/columnstore/mysql/my.cnf -uroot -vvv -Bse "set sql_mode=NO_ENGINE_SUBSTITUTION;GRANT ALL ON *.* to root@'"'"'%'"'"';FLUSH PRIVILEGES;"'
+# ssh -vvv -o "StrictHostKeyChecking=no" -l root ${UM_NODE_IP} '/usr/local/mariadb/columnstore/mysql/bin/mysql --defaults-file=/usr/local/mariadb/columnstore/mysql/my.cnf -uroot -vvv -Bse "set sql_mode=NO_ENGINE_SUBSTITUTION;GRANT ALL ON *.* to root@'"'"'%'"'"';FLUSH PRIVILEGES;"'
 
 # don't exit the process
 tail -f /dev/null
