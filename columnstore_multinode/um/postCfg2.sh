968f9b724dc8bd1d309d8f4d287953ed4cf9c95f
Petko Vasilev <petko.vasilev@mariadb.com>
2018-07-12 14:45:04 +0300

    added versions for running in docker-compose

--- columnstore_multinode/um/postCfg2.sh
+++ columnstore_multinode/um/postCfg2.sh
@@ -7,5 +7,8 @@ export USER=root
 # update root user to allow external connection, need to turn off NO_AUTO_CREATE_USER. 
 ssh -vvv -o "StrictHostKeyChecking=no" -l root mariadb-cs-um '/usr/local/mariadb/columnstore/mysql/bin/mysql --defaults-file=/usr/local/mariadb/columnstore/mysql/my.cnf -uroot -vvv -Bse "set sql_mode=NO_ENGINE_SUBSTITUTION;GRANT ALL ON *.* to root@'"'"'%'"'"';FLUSH PRIVILEGES;"'
 
+# for docker-compose
+# ssh -vvv -o "StrictHostKeyChecking=no" -l root ${UM_NODE_IP} '/usr/local/mariadb/columnstore/mysql/bin/mysql --defaults-file=/usr/local/mariadb/columnstore/mysql/my.cnf -uroot -vvv -Bse "set sql_mode=NO_ENGINE_SUBSTITUTION;GRANT ALL ON *.* to root@'"'"'%'"'"';FLUSH PRIVILEGES;"'
+
 # don't exit the process
 tail -f /dev/null
