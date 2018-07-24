c50c2b0e73e1e10b515efef3952c3f6d6243a192
Petko Vasilev <petko.vasilev@mariadb.com>
2018-07-24 19:07:36 +0300

    preparing for statefull set

--- galera/galera/entrypoint.sh
+++ galera/galera/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ex
 
 /etc/init.d/ssh start
 
@@ -8,10 +9,18 @@ chmod 666 /var/log/mysql/slow-query.log
 touch /var/log/mysql/error.log
 chmod 666 /var/log/mysql/error.log
 
-if [ "$MAIN_NODE" = true ] ; then
-    service mysql start --wsrep-new-cluster
-    mysql -vvv -Bse "set sql_mode=NO_ENGINE_SUBSTITUTION; GRANT ALL ON *.* to root@'%';FLUSH PRIVILEGES;"
-    mysql -vvv -Bse "alter user 'root'@'%' identified by '${MYSQL_PASS}'; FLUSH PRIVILEGES;"
+[[ $(hostname) =~ -([0-9]+)$ ]] || exit 1
+server_id=${BASH_REMATCH[1]}
+
+if [[ $server_id -eq 0 ]]; then
+    if [[ ! -f /var/lib/mysql/mysql/multi-master.info ]]; then
+        # first master run
+        service mysql start --wsrep-new-cluster
+        mysql -vvv -Bse "set sql_mode=NO_ENGINE_SUBSTITUTION; GRANT ALL ON *.* to root@'%';FLUSH PRIVILEGES;"
+        mysql -vvv -Bse "alter user 'root'@'%' identified by '${MYSQL_PASS}'; FLUSH PRIVILEGES;"
+    else
+        service mysql start
+    fi
 else
     service mysql start
 fi
