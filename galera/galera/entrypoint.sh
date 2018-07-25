ad4b9f8b27c2b2ddeab376467b1306057f2d375c
Petko Vasilev <petko.vasilev@mariadb.com>
2018-07-25 17:33:44 +0300

    added a galera statefulset

--- galera/galera/entrypoint.sh
+++ galera/galera/entrypoint.sh
@@ -13,15 +13,32 @@ chmod 666 /var/log/mysql/error.log
 server_id=${BASH_REMATCH[1]}
 
 if [[ $server_id -eq 0 ]]; then
-    if [[ ! -f /var/lib/mysql/mysql/multi-master.info ]]; then
+    if [[ ! -d /var/lib/mysql/mysql ]]; then
         # first master run
+        mysql_install_db --user=mysql --datadir=/var/lib/mysql
+
         service mysql start --wsrep-new-cluster
+        # fix debian account password
+        DEB_PASS=$(grep -i "password" /etc/mysql/debian.cnf | head -1 | awk 'match($0, "^password = (.*)$", m) {print m[1]}')
+        mysql -vvv -Bse "GRANT ALL PRIVILEGES ON *.* TO 'debian-sys-maint'@'localhost' IDENTIFIED BY '${DEB_PASS}';FLUSH PRIVILEGES;"
+
         mysql -vvv -Bse "set sql_mode=NO_ENGINE_SUBSTITUTION; GRANT ALL ON *.* to root@'%';FLUSH PRIVILEGES;"
         mysql -vvv -Bse "alter user 'root'@'%' identified by '${MYSQL_PASS}'; FLUSH PRIVILEGES;"
     else
-        service mysql start
+        PING0=$(mysql --host=galera-0.mariadb-cluster -Bse "SELECT 1" -p${MYSQL_PASS}) || PING0=0
+        PING1=$(mysql --host=galera-1.mariadb-cluster -Bse "SELECT 1" -p${MYSQL_PASS}) || PING1=0
+        PING2=$(mysql --host=galera-2.mariadb-cluster -Bse "SELECT 1" -p${MYSQL_PASS}) || PING2=0
+
+        if [[ $PING0 -eq 1 ]] || [[ $PING1 -eq 1 ]] || [[ $PING2 -eq 1 ]]; then
+            service mysql start
+        else
+            service mysql start --wsrep-new-cluster
+        fi
     fi
 else
+    if [[ ! -d /var/lib/mysql/mysql ]]; then
+        mysql_install_db --user=mysql --datadir=/var/lib/mysql
+    fi
     service mysql start
 fi
 
