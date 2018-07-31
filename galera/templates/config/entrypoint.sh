c49c9027d06d61e376ded638e1f3abf90f84abb0
Petko Vasilev <petko.vasilev@mariadb.com>
2018-07-31 14:27:06 +0300

    fixed starting the cluster after all instances are stopped

--- galera/templates/config/entrypoint.sh
+++ galera/templates/config/entrypoint.sh
@@ -3,21 +3,23 @@ set -ex
 
 /etc/init.d/ssh start
 
-# fix file permissions for fluentd
-touch /var/log/mysql/slow-query.log
-chmod 666 /var/log/mysql/slow-query.log
-touch /var/log/mysql/error.log
-chmod 666 /var/log/mysql/error.log
-
 [[ $(hostname) =~ -([0-9]+)$ ]] || exit 1
 server_id=${BASH_REMATCH[1]}
 
 if [[ $server_id -eq 0 ]]; then
     if [[ ! -d /var/lib/mysql/mysql ]]; then
-        # first master run
+        # fix file permissions for fluentd
+        touch /var/log/mysql/slow-query.log
+        chmod 666 /var/log/mysql/slow-query.log
+        touch /var/log/mysql/error.log
+        chmod 666 /var/log/mysql/error.log
+
+        # init database
         mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
+        # start mysql and create new cluster
         service mysql start --wsrep-new-cluster
+
         # fix debian account password
         DEBIAN_PASS=$(grep -i "password" /etc/mysql/debian.cnf | head -1 | awk 'match($0, "^password = (.*)$", m) {print m[1]}')
         mysql -vvv -Bse "GRANT ALL PRIVILEGES ON *.* TO 'debian-sys-maint'@'localhost' IDENTIFIED BY '${DEBIAN_PASS}';FLUSH PRIVILEGES;"
@@ -34,22 +36,36 @@ if [[ $server_id -eq 0 ]]; then
         mysql -vvv -Bse "GRANT ALL ON *.* TO '{{ADMIN_USER}}'@'%' WITH GRANT OPTION;"
 
         mysql -vvv -Bse "FLUSH PRIVILEGES;"
-
     else
+        # check if any other instance is up
         PING0=$(mysql --host={{APPLICATION}}-{{ENVIRONMENT}}-galera-0.{{APPLICATION}}-{{ENVIRONMENT}}-mariadb-galera -Bse "SELECT 1" -u{{ADMIN_USERNAME}} -p{{ADMIN_PASSWORD}}) || PING0=0
         PING1=$(mysql --host={{APPLICATION}}-{{ENVIRONMENT}}-galera-1.{{APPLICATION}}-{{ENVIRONMENT}}-mariadb-galera -Bse "SELECT 1" -u{{ADMIN_USERNAME}} -p{{ADMIN_PASSWORD}}) || PING1=0
         PING2=$(mysql --host={{APPLICATION}}-{{ENVIRONMENT}}-galera-2.{{APPLICATION}}-{{ENVIRONMENT}}-mariadb-galera -Bse "SELECT 1" -u{{ADMIN_USERNAME}} -p{{ADMIN_PASSWORD}}) || PING2=0
 
         if [[ $PING0 -eq 1 ]] || [[ $PING1 -eq 1 ]] || [[ $PING2 -eq 1 ]]; then
+            # start mysql
             service mysql start
         else
+            # start mysql and create new cluster
+            rm /var/lib/mysql/galera.cache
+            rm /var/lib/mysql/grastate.dat
+            rm /var/lib/mysql/gvwstate.dat
             service mysql start --wsrep-new-cluster
         fi
     fi
 else
     if [[ ! -d /var/lib/mysql/mysql ]]; then
+        # fix file permissions for fluentd
+        touch /var/log/mysql/slow-query.log
+        chmod 666 /var/log/mysql/slow-query.log
+        touch /var/log/mysql/error.log
+        chmod 666 /var/log/mysql/error.log
+
+        # init database
         mysql_install_db --user=mysql --datadir=/var/lib/mysql
     fi
+
+    # start mysql
     service mysql start
 fi
 
