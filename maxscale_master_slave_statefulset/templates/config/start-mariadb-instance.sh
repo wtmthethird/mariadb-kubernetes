53db7d93fbb00cd5605c608603d396404c28bdb2
Alexander Aldev <alexander.aldev@mariadb.com>
2018-08-03 16:38:31 +0300

    auto failover and rejoin works

--- maxscale_master_slave_statefulset/templates/config/start-mariadb-instance.sh
+++ maxscale_master_slave_statefulset/templates/config/start-mariadb-instance.sh
@@ -10,15 +10,16 @@ set -ex
 server_id=${BASH_REMATCH[1]}
 
 # check if we already are attached to DB storage
-if [[ ! -d /var/lib/mysql/mysql ]]; then
-   if [[ "{{MASTER_HOST}}" == "NO MASTER" ]]; then
-      # this is the master and it's the first run, ensure maxscale user is initialized
-      cp /mnt/config-map/users.sql /docker-entrypoint-initdb.d
-   else
+#if [[ ! -d /var/lib/mysql/mysql ]]; then
+#   if [[ "{{MASTER_HOST}}" == "NO MASTER" ]]; then
+#      # this is the master and it's the first run, ensure maxscale user is initialized
+#      cp /mnt/config-map/users.sql /docker-entrypoint-initdb.d
+#   else
       # a first run on a slave
-      cp /mnt/config-map/replication.sql /docker-entrypoint-initdb.d
-   fi
-fi
+#      mysqldump -h "{{MASTER_HOST}}" -u {{REPLICATION_USERNAME}} -p{{REPLICATION_PASSWORD}} --all-databases -A -Y --add-drop-database --add-drop-table --add-drop-trigger --allow-keywords --compact --master-data --lock-all-tables -F --flush-privileges --gtid -Q > /docker-entrypoint-initdb.d/slave.sql
+#      cat /mnt/config-map/replication.sql >> /docker-entrypoint-initdb.d/slave.sql
+#   fi
+#fi
 
 # fire up the instance
-/usr/local/bin/docker-entrypoint.sh mysqld --log-bin=mariadb-bin --binlog-format=ROW --server-id=$((3000 + $server_id))
+/usr/local/bin/docker-entrypoint.sh mysqld --log-bin=mariadb-bin --binlog-format=ROW --server-id=$((3000 + $server_id)) --log-slave-updates=1 --gtid-strict-mode=1
