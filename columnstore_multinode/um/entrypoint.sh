445bb590c2baee4d4fa6fcdeabd1b9ba139bfa8b
Petko Vasilev <petko.vasilev@mariadb.com>
2018-08-06 16:59:12 +0300

    made more robust ip detection

--- columnstore_multinode/um/entrypoint.sh
+++ columnstore_multinode/um/entrypoint.sh
@@ -8,13 +8,14 @@ set -ex
 server_id=${BASH_REMATCH[1]}
 
 if [[ $server_id -eq 2 ]]; then
-    sleep 10
+    sleep 5
+    
     export USER=root
-
+    OWN_ADDRESS=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
     /usr/sbin/sshd
 
     # postConfigure with input passed in
-    /bin/echo -e "2\n1\n\n\ncolumnstore-1\n\n1\ncolumnstore-0.mariadb-columnstore\n\n\n2\ncolumnstore-2.mariadb-columnstore\n\n\n\ncolumnstore-1.mariadb-columnstore\n\n\n2\n\n\n" | /usr/local/mariadb/columnstore/bin/postConfigure -n
+    /bin/echo -e "2\n1\n\n\ncolumnstore-1\n\n1\ncolumnstore-0.mariadb-columnstore\n\n\n2\ncolumnstore-2.mariadb-columnstore\n$OWN_ADDRESS\n\n\ncolumnstore-1.mariadb-columnstore\n\n\n2\n\n\n" | /usr/local/mariadb/columnstore/bin/postConfigure -n
 
     # update root user to allow external connection, need to turn off NO_AUTO_CREATE_USER. 
     ssh -vvv -o "StrictHostKeyChecking=no" -l root columnstore-0.mariadb-columnstore '/usr/local/mariadb/columnstore/mysql/bin/mysql --defaults-file=/usr/local/mariadb/columnstore/mysql/my.cnf -uroot -vvv -Bse "set sql_mode=NO_ENGINE_SUBSTITUTION;GRANT ALL ON *.* to root@'"'"'%'"'"';FLUSH PRIVILEGES;"'
