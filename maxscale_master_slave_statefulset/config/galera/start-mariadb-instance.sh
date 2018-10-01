429b508639767cf0c158dff37d697ca4d5250f70
Petko Vasilev <petko.vasilev@mariadb.com>
2018-10-01 18:31:15 +0300

    galera helm and state server integration (not quite working)

---
+++ maxscale_master_slave_statefulset/config/galera/start-mariadb-instance.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+# Copyright (C) 2018, MariaDB Corporation
+# 
+# Starts and initializes a MariaDB master or slave instance
+set -ex
+
+# get server id from hostname, it will have the format <something>-<id>
+[[ $(hostname) =~ -([0-9]+)$ ]] || exit 1
+server_id=${BASH_REMATCH[1]}
+MASTER_HOST=$(cat /mnt/config-map/master)
+
+cp /mnt/config-map/galera.cnf /etc/mysql/mariadb.conf.d/galera.cnf
+
+# fire up the instance
+if [[ "$MASTER_HOST" == "localhost" ]]; then
+    echo "starting master"
+    /usr/local/bin/docker-entrypoint.sh mysqld  --wsrep-new-cluster --log-bin=mariadb-bin --binlog-format=ROW --server-id=$((3000 + $server_id)) --log-slave-updates=1 --gtid-strict-mode=1
+else
+    echo "starting slave"
+    # rm -f /var/lib/mysql/galera.cache
+    # rm -f /var/lib/mysql/grastate.dat
+    # rm -f /var/lib/mysql/gvwstate.dat
+    /usr/local/bin/docker-entrypoint.sh mysqld --log-bin=mariadb-bin --binlog-format=ROW --server-id=$((3000 + $server_id)) --log-slave-updates=1 --gtid-strict-mode=1
+fi
