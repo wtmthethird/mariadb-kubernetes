6ceac97075849814e46ef8859c3bd8d0edd0dd61
Ivan Zlatoustov <ivan.zlatoustov@mariadb.com>
2018-12-12 17:38:59 +0200

    headed again

--- mariadb-enterprise/config/init-zeppelin.sh
+++ mariadb-enterprise/config/init-zeppelin.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+RELEASE_NAME={{ .Release.Name }}
+DATABASE_SERVICE="${RELEASE_NAME}-mdb-cs-um-module"
 mkdir -p /zeppelin/notebook
 if [ ! -f "/zeppelin/notebook/notebook.tar" ]; then
     echo "Getting the notebook archive ..."
@@ -6,4 +8,5 @@ if [ ! -f "/zeppelin/notebook/notebook.tar" ]; then
     echo "Extracting notebook files ..."
     tar -xf /zeppelin/notebook/notebook.tar --directory /zeppelin/notebook
 fi
-#find /zeppelin/notebook -type f -name note.json -exec sed -i "s/{columnstore_host_nm}/$APPLICATION-$ENVIRONMENT-mcs-sandbox/g" {} +
+if [[ "$CLUSTER_TOPOLOGY" == "galera" ]]; then
+find /zeppelin/notebook -type f -name note.json -exec sed -i "s/{columnstore_host_nm}/$DATABASE_SERVICE/g" {} +
